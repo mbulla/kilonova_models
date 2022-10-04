af944fa3f2a0d03555d66e58448ad553e2c99180
Mattia Bulla <mattia.bull@gmail.com>
2022-10-04 16:38:26 +0200

    doSmoothing option temporarily disabled due to early-time weirdness in some filters

--- make_lcs.py
+++ make_lcs.py
@@ -9,14 +9,14 @@ from scipy.interpolate import interpolate as interp
 
 def parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--filters',type=str,default='sdss::u,sdss::g,sdss::r,sdss::i,sdss::z,swope2::y,swope2::J,swope2::H,cspk',
+    parser.add_argument('--filters',type=str,default='sdss::u,sdss::g,sdss::r,sdss::i,sdss::z,swope2::y,swope2::J,swope2::H,cspk,bessellux,bessellb,bessellv,bessellr,besselli',
     help='comma-separated list of filters for photometric lcs; must be from the bandpasses listed here: \
     https://sncosmo.readthedocs.io/en/stable/bandpass-list.html')
     parser.add_argument('--modeldir',type=str,help='directory where .txt files are located',default='model/')
     parser.add_argument('--lcdir',type=str,help='output directory for generated lightcurves',default='lcs/')
     parser.add_argument('--doLbol',help='extract bolometric lightcurves',action="store_true",default=False)
     parser.add_argument('--doAB',help='extract photometric lightcurves',action="store_true",default=False)
-    parser.add_argument('--doSmoothing',help='Employ Savitzky-Golay filter for smoothing',action="store_true",default=False)
+    #parser.add_argument('--doSmoothing',help='Employ Savitzky-Golay filter for smoothing',action="store_true",default=False)
     parser.add_argument('--dMpc',type=float,help='distance in Mpc, default is 10 pc to get lightcurves in Absolute Mag',default=1e-5)
 
     return parser.parse_args()
@@ -91,12 +91,12 @@ for kk,filename in enumerate(files):
                 m = source.bandmag(filters[ifilt],"ab",ph)
                 
                 # apply smoothing filter
-                if args.doSmoothing:
-                    ii = np.where(~np.isnan(m))[0]
-                    if len(ii) > 1:
-                        f = interp.interp1d(ph[ii], m[ii], fill_value='extrapolate')
-                        m = f(ph)
-                    m = savgol_filter(m,window_length=17,polyorder=3)
+                #if args.doSmoothing:
+                #    ii = np.where(~np.isnan(m))[0]
+                #    if len(ii) > 1:
+                #        f = interp.interp1d(ph[ii], m[ii], fill_value='extrapolate')
+                #        m = f(ph)
+                #    m = savgol_filter(m,window_length=17,polyorder=3)
 
                 m_tot.append(m)
 
@@ -114,11 +114,11 @@ for kk,filename in enumerate(files):
 
             Lbol = np.trapz(fl*(4*np.pi*D_cm**2),x=wave)
 
-            if args.doSmoothing:
-                ii = np.where(np.isfinite(np.log10(Lbol)))[0]
-                f = interp.interp1d(ph[ii], np.log10(Lbol[ii]), fill_value='extrapolate')
-                Lbol = 10**f(ph)
-                Lbol = savgol_filter(Lbol,window_length=17,polyorder=3)
+            #if args.doSmoothing:
+            #    ii = np.where(np.isfinite(np.log10(Lbol)))[0]
+            #    f = interp.interp1d(ph[ii], np.log10(Lbol[ii]), fill_value='extrapolate')
+            #    Lbol = 10**f(ph)
+            #    Lbol = savgol_filter(Lbol,window_length=17,polyorder=3)
 
             for i,t in enumerate(ph):
                 Lbol_f.write(f'{t:.3f} {Lbol[i]:.5e} \n')
